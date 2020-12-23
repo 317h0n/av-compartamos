eca29ca212c0ffd0c1ef9bf2430a591c6a778004
Elthon Chocano Pareja <elthonchocano@gmail.com>
2020-12-22 22:35:08 -0500

    Iniciando Parte02

--- Parte01/src/main/MoneyParts.java
+++ Parte01/src/main/MoneyParts.java
@@ -3,8 +3,18 @@ package main;
 public class MoneyParts {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String entrada = "";
+		String salida = build(entrada);
+	}
 
+	private static String build(String entrada) {
+		Double total = Double.valueOf(entrada);
+		return obtenerCambio(total);
+	}
+
+	private static String obtenerCambio(Double total) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
